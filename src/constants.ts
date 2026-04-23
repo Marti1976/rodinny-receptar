3a1d7fd4b65d3ff48fa0feea5d248979b1043699
Marti1976 <125206366+Marti1976@users.noreply.github.com>
2026-04-23 21:36:19 +0200

    Add files via upload

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = "0.54.2";
-export const APP_CACHE_VERSION = "0.54.2";
+export const APP_VERSION = "0.54.3";
+export const APP_CACHE_VERSION = "0.54.3";
 export const APP_PASSWORD_HASH = "8364bda8231dd76a872334cab7e42f3f09285cd10d0f27395903d1fe08b6b6c1";
